db0eed5592ddbe26d23e87bb2be2de90b02b8023
VarvaraBorodina <vava.borodina@mail.ru>
2022-02-17 23:00:00 +0300

    First commit

---
+++ binary_tree/binary_tree.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+#include <cmath>
+#include <fstream>
+#include <string>
+using namespace std;
+
+struct Node
+{
+	int item;
+	Node* left;
+	Node* right;
+};
+
+Node* tree = NULL;
+ifstream input("input.txt");
+ofstream output("output.txt");
+
+void pushItem(Node** tree, int item) {
+	if (*tree) {
+		if (item < (*tree)->item) {
+			pushItem(&(*tree)->left, item);
+		}
+		if (item > (*tree)->item) {
+			pushItem(&(*tree)->right, item);
+		}
+	}
+	else {
+		(*tree) = new Node;
+		(*tree)->item = item;
+		(*tree)->left = NULL;
+		(*tree)->right = NULL;
+	}
+}
+
+void print(Node* tree)
+{
+	if (tree) {
+		output << tree->item << endl;
+		print(tree->left);
+		print(tree->right);
+	}
+}
+
+int main()
+{
+	string line;
+	if (input.is_open()) cout << "ooo" << endl;
+
+	while (getline(input, line))
+	{
+		pushItem(&tree, stoi(line));
+	}
+
+	input.close();
+	print(tree);
+}
